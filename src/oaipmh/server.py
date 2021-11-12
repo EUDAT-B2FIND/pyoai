03c790431d96024b29f3cd8575602d996350ff80
Lenzma <fluegel@dkrz.de>
2021-11-12 17:03:32 +0100

    patched for python 2.7.

--- src/oaipmh/server.py
+++ src/oaipmh/server.py
@@ -451,7 +451,7 @@ def encodeResumptionToken(kw, cursor):
     if until is not None:
         kw['until'] = datetime_to_datestamp(until)
     #return quote(urlencode(kw))
-    return base64.b64encode(bytes(urlencode(kw), 'utf-8'))
+    return base64.b64encode(str(urlencode(kw)))
 
 def decodeResumptionToken(token):
     #token = str(unquote(token))
